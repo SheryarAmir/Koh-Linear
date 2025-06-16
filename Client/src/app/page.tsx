4e3dc257180600d3d30d408ee145cbcc811e1dc1
SheryarAmir <dev.sheryaramir@gmail.com>
2025-06-16 21:50:22 +0500

    setup project structure install packages

---
+++ Client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, iste praesentium sit provident odio quas excepturi hic temporibus labore eaque? Neque voluptas sed quibusdam praesentium quasi quae vero perferendis id?
+    </div>
+  );
+}
